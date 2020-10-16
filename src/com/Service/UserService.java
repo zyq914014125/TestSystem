69a58b9803d503a746435fdfc1b2eb58c17e40c5
zyq914014125 <914014125@qq.com>
2020-10-16 11:18:46 +0800

    Initial commit

---
+++ src/com/Service/UserService.java
@@ -0,0 +1,16 @@
+package com.Service;
+
+import com.Dao.UserDao;
+import com.Dao.impl.UserDaoImpl;
+import com.Entity.User;
+
+import java.util.Random;
+
+public class UserService {
+    private UserDao userDao=new UserDaoImpl();
+
+    public User queryUser(String username, String password) {
+        return userDao.queryUser(username,password);
+    }
+
+}
